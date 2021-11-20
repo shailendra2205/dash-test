02fb628fe22dc94bbd1b027240d3b07514750a6f
shailendra2205 <shailendra88@gmail.com>
2021-11-20 22:19:40 +0400

    documentation and requirements freeze

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+"""This is a simple dash app that visualizes two graphs using plotly.
+The dataset used is the Barley dataset which is imported using the
+ vega-datasets python package from the altair library.
+"""
+
+import logging
+
 import dash
 from dash import html
 import dash_core_components as dcc
@@ -7,27 +14,34 @@ import pandas as pd
 from utilities.graph import generate_stacked_line_plot, generate_stacked_bar_plot
 from utilities.data import fetch_data_source
 
-import logging
 
-logging.basicConfig(filename='logs.log',level=logging.INFO)
+logging.basicConfig(filename="logs.log", level=logging.INFO)
 
 app = dash.Dash(__name__)
 
+# fetch the data source from the vega-datasets
 source = fetch_data_source()
+
+# if data is fetched successfully, render the two graphs
 if isinstance(source, pd.DataFrame):
     app.logger.info("Data imported successfully")
+
+    # generate the graphs from the data source
     fig_median_of_yield_vs_year = generate_stacked_line_plot(data=source)
     fig_sum_of_yield_vs_variety = generate_stacked_bar_plot(data=source)
-    app.layout = html.Div([
-        dcc.Graph(figure=fig_median_of_yield_vs_year),
-        dcc.Graph(figure=fig_sum_of_yield_vs_variety)
-    ])
+
+    app.layout = html.Div(
+        [
+            dcc.Graph(figure=fig_median_of_yield_vs_year),
+            dcc.Graph(figure=fig_sum_of_yield_vs_variety),
+        ]
+    )
+
+# else render an error message
 else:
     app.logger.error("Failed to import data")
-    app.layout = html.Div([
-        html.H1("Failed to load Data")
-    ])
+    app.layout = html.Div([html.H1("Failed to load Data")])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
--- utilities/graph.py
+++ utilities/graph.py
@@ -1,88 +1,119 @@
+"""This module contains functions that use plotly express to visualize the
+two graphs using the provided data and returns the figures to the dash app.
+"""
+
 import plotly.express as px
 
+
 def generate_stacked_line_plot(data):
-    df = data
-    df = df.groupby(['year', 'site']).agg({'yield': 'median'})
-    df.columns = ['Median of yield']
-    df = df.reset_index()
-    fig = px.line(df, x="year", y="Median of yield", color="site", 
+
+    """This function generates the stacked line chart"""
+
+    data_df = data
+    data_df = data_df.groupby(["year", "site"]).agg({"yield": "median"})
+    data_df.columns = ["Median of yield"]
+    data_df = data_df.reset_index()
+
+    fig = px.line(
+        data_df,
+        x="year",
+        y="Median of yield",
+        color="site",
         title="Median of Site Yields vs. Year",
-        width=300, height=600,
-        labels={ # replaces default labels by column name
-            "year": "<b>year</b>",  "Median of yield": "<b>Median of yield</b>", "site": "<b>site</b>"
+        width=300,
+        height=600,
+        labels={  # replaces default labels by column name
+            "year": "<b>year</b>",
+            "Median of yield": "<b>Median of yield</b>",
+            "site": "<b>site</b>",
         },
-        category_orders={ # replaces default order by column name
-            "site": sorted(df['site'].unique().tolist(), reverse=False)
+        category_orders={  # replaces default order by column name
+            "site": sorted(data_df["site"].unique().tolist(), reverse=False)
         },
-        color_discrete_map={ # replaces default color mapping by value
-            "Crookston": "#5578A4", "Duluth": "#E78A39", "Grand Rapids": "#D4605B", "Morris": "#81B5B2", "University Farm": "#689F55", "Waseca": "#E9CA58"
+        color_discrete_map={  # replaces default color mapping by value
+            "Crookston": "#5578A4",
+            "Duluth": "#E78A39",
+            "Grand Rapids": "#D4605B",
+            "Morris": "#81B5B2",
+            "University Farm": "#689F55",
+            "Waseca": "#E9CA58",
         },
-        template="simple_white"
-        )
-    fig.update_yaxes( # the y-axis is in dollars
-        #tickprefix="$",
-        tickmode = 'linear',
-        range = [0, 55],
-        tick0 = 0,
-        dtick = 5,
-        showgrid=True
+        template="simple_white",
     )
-    fig.update_xaxes( # the y-axis is in dollars
-        #tickprefix="$",
-        tickmode = 'linear',
+
+    fig.update_yaxes(
+        tickmode="linear",
+        range=[0, 55],
+        tick0=0,
+        dtick=5,
+        showgrid=True,
+    )
+
+    fig.update_xaxes(
+        tickmode="linear",
         range=[1930.5, 1932.5],
-        dtick = 1,
+        dtick=1,
         showgrid=False,
-        tickangle=-90
+        tickangle=-90,
     )
-    # fig.update_layout(
-    #     margin=dict(l=0, r=10, b=0, pad=0)
-    #     )
+
     return fig
 
+
 def generate_stacked_bar_plot(data):
-    df = data
-    df = df.groupby(['variety', 'site']).agg({'yield': 'sum'})
-    df.columns = ['Sum of yield']
-    df['Sum of yield'] = df['Sum of yield'].apply(lambda x: round(x,2))
-    df.sort_values('variety', ascending=False, inplace=True)
-    df.sort_values('site', ascending=False, inplace=True)
-    df = df.reset_index()
-    fig = px.bar(df, x="variety", y="Sum of yield", color="site",
-            title="Sum of Yield vs. Variety with Site breakdown",
-            width=500, height=600,
-            labels={ # replaces default labels by column name
-                "variety": "<b>variety</b>",  "Sum of yield": "<b>Sum of yield</b>", "site": "<b>site</b>"
-            },
-            category_orders={ # replaces default order by column name
-                "variety": sorted(df['variety'].unique().tolist()),
-                "site": sorted(df['site'].unique().tolist(), reverse=True)
-            },
-            color_discrete_map={ # replaces default color mapping by value
-                "Crookston": "#5578A4", "Duluth": "#E78A39", "Grand Rapids": "#D4605B", "Morris": "#81B5B2", "University Farm": "#689F55", "Waseca": "#E9CA58"
-            },
-            template="simple_white"
-            )
+
+    """This function generates the stacked bar chart"""
+
+    data_df = data
+    data_df = data_df.groupby(["variety", "site"]).agg({"yield": "sum"})
+    data_df.columns = ["Sum of yield"]
+    data_df["Sum of yield"] = data_df["Sum of yield"].apply(lambda x: round(x, 2))
+    data_df.sort_values("variety", ascending=False, inplace=True)
+    data_df.sort_values("site", ascending=False, inplace=True)
+    data_df = data_df.reset_index()
+
+    fig = px.bar(
+        data_df,
+        x="variety",
+        y="Sum of yield",
+        color="site",
+        title="Sum of Yield vs. Variety with Site breakdown",
+        width=500,
+        height=600,
+        labels={  # replaces default labels by column name
+            "variety": "<b>variety</b>",
+            "Sum of yield": "<b>Sum of yield</b>",
+            "site": "<b>site</b>",
+        },
+        category_orders={  # replaces default order by column name
+            "variety": sorted(data_df["variety"].unique().tolist()),
+            "site": sorted(data_df["site"].unique().tolist(), reverse=True),
+        },
+        color_discrete_map={  # replaces default color mapping by value
+            "Crookston": "#5578A4",
+            "Duluth": "#E78A39",
+            "Grand Rapids": "#D4605B",
+            "Morris": "#81B5B2",
+            "University Farm": "#689F55",
+            "Waseca": "#E9CA58",
+        },
+        template="simple_white",
+    )
+
     fig.update_yaxes(
-        tickmode = 'linear',
-        linewidth=0,
-        range = [0, 500],
-        tick0 = 0,
-        dtick = 50,
-        showgrid=True
+        tickmode="linear", linewidth=0, range=[0, 500], tick0=0, dtick=50, showgrid=True
     )
-    fig.update_xaxes(
-        showgrid=False,
-        tickangle=-90
-    )
+
+    fig.update_xaxes(showgrid=False, tickangle=-90)
+
     fig.update_layout(
-        legend={'traceorder': 'reversed'},
-        barmode='relative',
+        legend={"traceorder": "reversed"},
+        barmode="relative",
         bargap=0.0,
         bargroupgap=0.2,
-        boxmode='group',
+        boxmode="group",
         boxgap=0.0,
         boxgroupgap=0.0,
-        # margin=dict(l=0, r=10, b=0, pad=0)
-        )
+    )
+
     return fig
