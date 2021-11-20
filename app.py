14b68574b8876610537b8f5b17780b67dafda32d
shailendra2205 <shailendra88@gmail.com>
2021-11-20 06:32:44 +0400

    import data

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import dash
 from dash import html
+from vega_datasets import data
+import pandas as pd
+
+source = data.barley()
+
+df = pd.read_csv(source)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1('Hello, World!')
+    html.H1('Hello, World!'+str(len(df)))
 ])
 
 if __name__ == '__main__':
